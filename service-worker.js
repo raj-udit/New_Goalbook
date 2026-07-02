811ce392e34ae85cfeadd5e4524b0ee7c7e2f1d9
raj-udit <86419062+raj-udit@users.noreply.github.com>
2026-07-02 19:37:23 +0530

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,23 @@
+const CACHE = "goalbook-v1";
+
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
